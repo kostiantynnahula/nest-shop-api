0ae4c0256532bf76db469143eff79d48e9b7a0da
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-08-13 11:41:11 +0300

    Generate new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ColorModule } from './color/color.module';
+import { CategoryModule } from './category/category.module';
+import { FileModule } from './file/file.module';
+import { StoreModule } from './store/store.module';
+import { OrderModule } from './order/order.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule, ColorModule, CategoryModule, FileModule, StoreModule, OrderModule, StatisticsModule],
 })
 export class AppModule {}
